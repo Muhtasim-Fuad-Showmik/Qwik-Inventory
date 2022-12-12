c66a77f3657547d3f11d87a5083d9a20745eae7c
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2022-12-12 17:01:20 +0600

    Prepared inset table within the procurement page

--- frontend/src/components/accordionTable/accordionTable.tsx
+++ frontend/src/components/accordionTable/accordionTable.tsx
@@ -23,7 +23,7 @@ export default component$((props: accordionTableProps) => {
         </tr>
       </thead>
       <tbody>
-        <tr>
+        <tr className="selected">
           <td className="orderId">#2323</td>
           <td>23/10/2022</td>
           <td>Julia Roberts</td>
@@ -42,6 +42,89 @@ export default component$((props: accordionTableProps) => {
             <div className="circle">
               <i class="fa-solid fa-chevron-down"></i>
             </div>
+          </td>
+        </tr>
+        <tr className="dependent">
+          <td colSpan={8}>
+            <table className="table secondary" cellSpacing="0">
+              <tbody>
+                <tr>
+                  <td>
+                    <img className="thumbnail" src="/Drone.jpg" alt="Drone" />
+                  </td>
+                  <td>
+                    <div className="item-header">DJI Mini 3 Pro</div>
+                    <div>DJI RC</div>
+                  </td>
+                  <td>
+                    <div>Pick</div>
+                    <div>1</div>
+                  </td>
+                  <td>
+                    <div>Bin</div>
+                    <div>C011-034</div>
+                  </td>
+                  <td>
+                    <div>Vendor</div>
+                    <div>DJI</div>
+                  </td>
+                  <td>
+                    <div>On Hand</div>
+                    <div>23</div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img className="thumbnail" src="/Gaming Chair.webp" alt="Gaming Chair" />
+                  </td>
+                  <td>
+                    <div className="item-header">GTRACING Gaming Chair with Footrest</div>
+                    <div>WMT-GT802A-WHITE</div>
+                  </td>
+                  <td>
+                    <div>Pick</div>
+                    <div>3</div>
+                  </td>
+                  <td>
+                    <div>Bin</div>
+                    <div>C023-6121</div>
+                  </td>
+                  <td>
+                    <div>Vendor</div>
+                    <div>GTRACING</div>
+                  </td>
+                  <td>
+                    <div>On Hand</div>
+                    <div>13</div>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img className="thumbnail" src="/Laptop Cooler.jpg" alt="drone" />
+                  </td>
+                  <td>
+                    <div className="item-header">Laptop Cooling Pad, Laptop Cooler with 6 Quiet Led Fans for 15.6-17 Inch Laptop Cooling Fan Stand</div>
+                    <div>19019154</div>
+                  </td>
+                  <td>
+                    <div>Pick</div>
+                    <div>2</div>
+                  </td>
+                  <td>
+                    <div>Bin</div>
+                    <div>LC71-023</div>
+                  </td>
+                  <td>
+                    <div>Vendor</div>
+                    <div>LIANGSTAR</div>
+                  </td>
+                  <td>
+                    <div>On Hand</div>
+                    <div>18</div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </td>
         </tr>
         <tr>
